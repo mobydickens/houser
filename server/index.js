e8f97a53c6b04ffd85de5239d337b7395c01c54f
mobydickens <steadmegan@gmail.com>
2018-12-07 10:03:35 -0700

    Finished database setup and inserted some dummy data for testing.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 let express = require('express');
 let bodyParser = require('body-parser');
+let massive = require('massive');
 let PORT = 4000;
+let { CONNECTION_STRING } = process.env;
 
 let app = express();
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`server is running at port ${PORT}`));
+massive(CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set('db', dbInstance);
+    app.listen(PORT, () => console.log(`server is running at port ${PORT}`));
+    console.log('connected to the db!')
+  });
+
